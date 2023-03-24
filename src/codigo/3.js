bdb10be4694197ac13159a692128a0e8520220cf
Moises Ayala <mayalam1991emanuel@gmail.com>
2023-03-24 00:07:48 -0600

    24/03/23 00:07

---
+++ src/codigo/3.js
@@ -0,0 +1,13 @@
+//a que tipo pertenece cada valor
+console.log(typeof(40)); //tipo numero
+typeof("q"); //tipo string
+typeof(true); //tipo boleean
+typeof(null); //tipo objeto
+typeof(undefined); //tipo undefined
+typeof([]); //tipo objeto
+typeof({}); //tipo objeto
+/*este 
+comentario
+tiene 
+varias
+lineas*/
